51497e76016798d5ff89f3851ccc5f33ffd5aa7f
Bayekula-hb <55344393+Bayekula-hb@users.noreply.github.com>
2024-08-11 21:31:29 +0100

    Refactor: change remote url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 
 const NgandaValue = {
-    URL_API_REMOTE : "http://64.23.250.250/api/v1/",
+    URL_API_REMOTE : "https://ngandamoni.com/api/v1/",
     URL_API_V1_1_REMOTE : "http://161.35.110.145/api/v1.1",
     URL_API_LOCAL : "http://nganda_api.test/api/v1.1/",
 }
